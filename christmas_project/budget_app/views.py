6d81fdbc7f1e3521de94dad9313c5b2e1d1cac94
aleksandrapieszak <aleksandraapieszak@gmail.com>
2020-12-19 17:06:25 +0100

    'Dzien01'

---
+++ christmas_project/budget_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+
+
+def start(request):
+    return render(request, 'index.html')
