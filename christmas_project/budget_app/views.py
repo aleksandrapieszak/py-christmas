251fe57db05c74c4057602484265e8ecf2d0e7ad
aleksandrapieszak <aleksandraapieszak@gmail.com>
2020-12-20 16:40:40 +0100

    'Dzien02'

--- christmas_project/budget_app/views.py
+++ christmas_project/budget_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views import View
 # Create your views here.
@@ -18,14 +18,16 @@ def all_family(request):
                     )
     else:
         family_member = request.POST['family_member']
+        request.session.get('family_member')
         new_family_member = request.POST['new_family_member']
-        if family_member == "New" and new_family_member.length > 2:
-            family_member_id = request.POST['new_family_member_id']
-            request.session['new_family_member_id'] = family_member_id
-            return render(request, 'login.html', {
-                'new_family_member': new_family_member,
-                'new_family_member_id': family_member_id
-            })
+        if family_member == "new" and len(new_family_member) > 2 and new_family_member != family_member:
+            new_dude = FamilyMember.objects.create(name=new_family_member)
+            new_dude_id = FamilyMember.objects.get(id=new_dude.id)
+            request.session['new_dude_id'] = new_dude_id
+            return redirect("/")
+        else:
+            raise Http404
+
 
 
 
